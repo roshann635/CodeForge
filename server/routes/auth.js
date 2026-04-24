fcb966c3bce7a781882e5474eae38efc80905f65
roshann635 <roshanjadhav4385@gmail.com>
2026-04-24 14:39:57 +0530

    authentication fixes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,7 +6,9 @@ const nodemailer = require('nodemailer');
 const router = express.Router();
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
